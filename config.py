5aceeb364127f6d2ed0e56d24a427a739329d3cf
manvillej <jeffmanvillejr@gmail.com>
2018-04-03 08:53:01 -0400

    adding database packages

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '8vd%4la%7TuP5Od3vG6DDaxTuU6X%U%aGRbiS6Xb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
