5ac0a98e966b0a6d5da95ec74f7faecf320da5d7
manvillej <jeffmanvillejr@gmail.com>
2018-06-11 23:41:42 -0400

    added User & Post data models and updated microblog for Flask Shell

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor # grinberg ch4, Shell Context
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
