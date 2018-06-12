b2f3eb0a90832f47c8cdd5f79cc84bd1fe4f3ebd
manvillej <jeffmanvillejr@gmail.com>
2018-06-11 22:50:51 -0400

    Adding database and migrations from chapter 4

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '8vd%4la%7TuP5Od3vG6DDaxTuU6X%U%aGRbiS6Xb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
