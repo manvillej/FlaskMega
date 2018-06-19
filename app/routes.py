2cc510092a319234fba15b0cee2279b84ae41604
manvillej <jeffmanvillejr@gmail.com>
2018-06-18 20:37:06 -0400

    Added profile route

--- app/routes.py
+++ app/routes.py
@@ -72,3 +72,15 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def profile(username):
+    # first() grabs the first record found in the db, first_or_404 throws a 404 error if nothing is found
+    user = User.query.filter_by(username=username).first_or_404() 
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
